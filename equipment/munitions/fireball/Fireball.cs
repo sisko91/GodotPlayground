37d0b45bf752ccb836b9b05775de6f14eef58828
sisko91 <86699085+sisko91@users.noreply.github.com>
2025-02-24 15:27:37 -0800

    Added fireball explode animation and sound

--- equipment/munitions/fireball/Fireball.cs
+++ equipment/munitions/fireball/Fireball.cs
@@ -1,4 +1,4 @@
-using First2DGame;
+        using First2DGame;
 using Godot;
 using System;
 
@@ -7,21 +7,12 @@ public partial class Fireball : Area2D, IMunition
     public Vector2 Velocity { get; set; }
     public Vector2 ScreenSize; // Size of the game window
 
-    [Export]
-    public NodePath AudioTrack { get; set; }
-
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
         ScreenSize = GetViewportRect().Size;
-
-        if(AudioTrack != null)
-        {
-            var track = GetNode<AudioStreamPlayer>(AudioTrack);
-            if (track != null) {
-                track.Play();
-            }
-        }
+        var track = GetNode<AudioStreamPlayer>("Fireball_Shoot");
+        track.Play();
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -35,7 +26,18 @@ public partial class Fireball : Area2D, IMunition
     }
 
     private void OnBodyEntered(Node2D body) {
-        body.QueueFree();
-        QueueFree();
+        var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        animatedSprite2D.Play();
+        animatedSprite2D.AnimationFinished += Hide;
+
+        var track = GetNode<AudioStreamPlayer>("Fireball_Explode");
+        //Doesn't work, framework bug maybe
+        //track.Finished += QueueFree;
+        //track.Finished += body.QueueFree;
+        track.Play();
+
+        var timer = GetTree().CreateTimer(0.15);
+        timer.Timeout += body.QueueFree;
+        timer.Timeout += QueueFree;
     }
 }
